84c0f75d1c1e3fc97935ddac3634bf471b23b38d
Henry <hpratt111@live.com>
2019-03-17 21:31:27 -0400

    update config for local server

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const xhttp = new XMLHttpRequest();
+let data = {};
+xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        try {
+            data = JSON.parse(xhttp.responseText);
+        } catch (e) {}
+	ReactDOM.render(<App config={data || {}} />, document.getElementById('root'));
+    }
+};
+
+xhttp.open('GET', `/config.json`, true);
+xhttp.send();
+
 registerServiceWorker();
