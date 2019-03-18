370cdf82e0cf198a6a0a8a52a86d63b98beea09c
Henry <hpratt111@live.com>
2019-03-17 21:31:27 -0400

    update config for local server

---
+++ webapp/scripts/run-local.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# arg1: environment used to pick config file to use in /config.
+set -e
+
+# cd to project root directory
+cd "$(dirname "$(dirname "$0")")"
+
+# Set environment variable (used for config) to staging by default
+[[ ! -z "$1" ]] && ENVIRONMENT="$1" || ENVIRONMENT=local
+
+echo config/config.${ENVIRONMENT}.json
+pwd
+#ln -sf config/config.${ENVIRONMENT}.json public/config.json
+cp config/config.${ENVIRONMENT}.json public/config.json
+yarn start
--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -15,12 +15,12 @@ class App extends Component {
 	    <Router>
 	      <Switch>
 		<Route exact path='/' component={Main} />
-		<Route path='/editor/pwm' render={() => <PWMWorkspace apiserver={this.props.apiserver} />} />
- 	        <Route path='/editor/fasta' render={() => <FastaWorkspace apiserver={this.props.apiserver} />} />
- 	        <Route path='/editor/meme' render={() => <MEMEWorkspace apiserver={this.props.apiserver} />} />
-                <Route path='/editor/jaspar' render={() => <JASPARWorkspace apiserver={this.props.apiserver} />} />
-                <Route path='/editor/transfac' render={() => <TRANSFACWorkspace apiserver={this.props.apiserver} />} />
-		<Route path='/editor/fastaupload' render={() => <FASTAUploadWorkspace apiserver={this.props.apiserver} />} />
+		<Route path='/editor/pwm' render={() => <PWMWorkspace apiserver={this.props.config.APIURL} />} />
+ 	        <Route path='/editor/fasta' render={() => <FastaWorkspace apiserver={this.props.config.APIURL} />} />
+ 	        <Route path='/editor/meme' render={() => <MEMEWorkspace apiserver={this.props.config.APIURL} />} />
+                <Route path='/editor/jaspar' render={() => <JASPARWorkspace apiserver={this.props.config.APIURL} />} />
+                <Route path='/editor/transfac' render={() => <TRANSFACWorkspace apiserver={this.props.config.APIURL} />} />
+		<Route path='/editor/fastaupload' render={() => <FASTAUploadWorkspace apiserver={this.props.config.APIURL} />} />
 	      </Switch>
 	    </Router>
 	);
