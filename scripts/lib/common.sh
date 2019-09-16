a7cdadaead304b3c3b587fa13e893416cee42d1c
hpratt <hpratt111@live.com>
2019-09-16 13:27:32 -0400

    CI deployment

--- scripts/lib/common.sh
+++ scripts/lib/common.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-DOCKER_ORG=genomealmanac
-IMAGE_NAME=logosj-webapp
+GCR_PROJECT_ID=devenv-215523
+DOCKER_IMAGE_NAME=logosj-webapp
+KUBE_DEPLOYMENT_NAME=logosj-webapp-deployment
+KUBE_DEPLOYMENT_CONTAINER_NAME=logosj-webapp
